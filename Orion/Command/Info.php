9e6bdbb9eb536b2711de560b01a95e6cad34d60c
tiago_moura <tiago4orion@gmail.com>
2009-10-21 16:49:18 +0000

    Refactoring

--- Orion/Command/Info.php
+++ Orion/Command/Info.php
@@ -1,18 +1,38 @@
 <?php
 /*
- * #################################################
- * #################################################
- * ##########         INTERFACE         ############
- * #################################################
- *
- * @author Tiago Natel de Moura
- * @email tiago_moura@live.com
- */
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License version 2 as published by the Free Software Foundation.
 
- interface OrionCommand_Info {
- 	// Método que retorna o método
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+
+    ---
+    Copyright (C) 2009, Tiago Natel de Moura tiago_moura@live.com
+*/
+
+/**
+ * OrionCommand_Info
+ * Interface para OrionCommand_Info_Rewrite, OrionCommand_Info_Default, etc
+ *
+ * @package     Orion
+ * @author      Tiago Moura <tiago_moura@live.com>
+ * @license     http://www.opensource.org/licenses/lgpl-license.php LGPL
+ * @link        www.orion-framework.org
+ * @since       1.0
+ * @version     $Revision: 1 $
+ */
+interface OrionCommand_Info {
+ 	/** Retorna o módulo */
  	public function getModule();
 
- 	// Método que retorna a Action
+ 	/** Retorna a Action */
  	public function getAction();
  }
