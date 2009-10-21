9e7a63b8e19ceab46861fc40af7500900f59c962
tiago_moura <tiago4orion@gmail.com>
2009-10-21 16:51:54 +0000

    Refactoring

---
+++ Orion/Error.php
@@ -0,0 +1,51 @@
+<?php
+/*
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License version 2 as published by the Free Software Foundation.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+
+    ---
+    Copyright (C) 2009, Tiago Natel de Moura tiago_moura@live.com
+*/
+
+/**
+ * Orion
+ * OrionError
+ *
+ * @package     Orion
+ * @author      Tiago Moura <tiago_moura@live.com>
+ * @license     http://www.opensource.org/licenses/lgpl-license.php LGPL
+ * @link        www.orion-framework.org
+ * @since       1.0
+ * @version     $Revision: 1 $
+ */
+
+class OrionError
+{
+	const NOT_DEFINED								= 0;
+	const CLASS_NOT_EXISTS 							= 1;
+	const METHOD_NOT_EXISTS							= 2;
+	const PROPERTY_OF_CLASS_NOT_EXISTS				= 3;
+	const FILE_NOT_WRITABLE							= 4;
+	const INCORRECT_DIR_PROJECT						= 5;
+	const FILE_NOT_EXISTS							= 6;
+	const PAGE_NOT_FOUND							= 7;
+	const SET_LIBRARY								= 8;
+	const TYPE_STRING								= 9;
+	const TYPE_INTEGER								= 10;
+	
+	/**
+	 * Erros PDO
+	 */
+	
+}
