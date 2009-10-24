4bf82696864db6ae01d49d0d43fb9a28689495a5
tiago_moura@live.com <tiago_moura@live.com@f976c7c2-a647-11de-889f-a79cef5dd62c>
2009-10-24 10:01:33 +0000

    Refactoring

--- trunk/Orion/Number.php
+++ trunk/Orion/Number.php
@@ -2,5 +2,23 @@
 
 abstract class OrionNumber
 {
-
+	public function intValue()
+	{
+		return (integer) $this->value;
+	}
+	
+	public function floatValue()
+	{
+		return (float) $this->value;
+	}
+	
+	public function doubleValue()
+	{
+		return (double) $this->value;
+	}
+	
+	public function byteValue()
+	{
+	
+	}
 }
